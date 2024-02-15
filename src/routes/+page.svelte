9b17d058aa49e68be00f0dbda71ef86ae9b67dd6
hanniboy <hanniboy@gmail.com>
2024-02-15 16:56:08 +0700

    standard commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,28 @@
-<enhanced:img src='/static/images/ardklima.jpg' alt="An alt text" sizes="min(400px, 50vw" />
+<div class="p-3 space-y-5">
+    <h1 class="">
+        Willkommen auf cdl-protokolle.com
+    </h1>
+    
+    
+    <h2>
+        Ihrer Quelle für hochwertige Gesundheitsinformationen und wertvolle Tipps für ein gesundes Leben! 
+    </h2>
+    
+    <div>
+         Wir freuen uns, Sie auf unserer Website begrüßen zu dürfen, wo wir Ihnen kostenlosen Zugang zu Leseproben aus einer Vielzahl erstklassiger Gesundheitsbücher bieten. Bei uns finden Sie zahlreiche Ratschläge und Informationen, wie Sie Ihre Gesundheit verbessern und erhalten können – und das alles ohne den Einsatz von Pharma-Medizin.
+    
+    </div>
+    <div>
+        Die Idee zu dieser Website entstand aus der gleichnamigen Telegram-Gruppe, in der wir zahlreiche Informationen sammeln und diskutieren. Unser Ziel ist es, diese Informationen noch besser zu organisieren und aufzubereiten, um sie Ihnen leicht zugänglich zu machen. Wir laden Sie herzlich ein, auch Mitglied unserer Telegram-Gruppe zu werden, um sich aktiv mit Gleichgesinnten auszutauschen und von weiteren Informationen zu profitieren. Sie können der Gruppe unter folgendem Link beitreten: https://t.me/cdl_protokolle.
+    
+    </div>
+    <div>
+        Tauchen Sie ein in die Welt der ganzheitlichen Gesundheit und entdecken Sie, wie Sie Ihr Wohlbefinden auf natürliche Weise fördern können. Unsere Website bietet Ihnen eine Fülle von Ressourcen, um Ihnen auf Ihrem Weg zu einem gesünderen Lebensstil zu helfen. Viel Spaß beim Lesen und Entdecken!
+    
+    </div>
+    
+    
+</div>
+
+
+
