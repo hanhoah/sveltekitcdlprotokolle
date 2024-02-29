ced75fef9e985d6346e3d63a590c967ae9ba0f48
hanniboy <hanniboy@gmail.com>
2024-02-29 15:20:44 +0700

    standard commit

--- src/routes/gai-goi-vermittlung/+page.svelte
+++ src/routes/gai-goi-vermittlung/+page.svelte
@@ -1,3 +1,11 @@
 
+<div class="p-3 space-y-5">
+    <h1 class="">
+        Willkommen bei den heissesten gai gois Vietnams
+    </h1>
+    <img src="/images/hai.jpg" />
+</div>
+        
+        
+      
 
-<img src="/images/hai.jpg" />
