e68b9ef2ee996eaec669a2bb3e9a2098e652c7f3
hanniboy <hanniboy@gmail.com>
2024-03-10 15:12:10 +0700

    standard commit

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,6 +2,3 @@ import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 
 inject({ mode: dev ? 'development' : 'production' });
-
-// This can be false if you're using a fallback (i.e. SPA mode)
-export const prerender = true;
--- src/routes/produkte/hashtag/[tag]/+page.ts
+++ src/routes/produkte/hashtag/[tag]/+page.ts
@@ -3,14 +3,11 @@ import {getHashtag} from '$lib/functions/getHashtags.ts';
 
 export async function load({ params }) {
 	console.log('hashtags:');
-	console.log('**********');
 	let products = [];
 	// Hier lädst du alle Produkte aus deiner Datenquelle, z.B. einer API
 
 	const hashtagid = params.tag;
 	const {tag: hashtag} = await getHashtag(hashtagid)
-	console.log('hastag id ', hashtagid);
-	console.log('hashtag ', hashtag);
 
 	products = await getProductsByName(hashtag);
 
