49c6cf2ae08f0401583dad11793a7bb1a0dfa649
hanniboy <hanniboy@gmail.com>
2024-03-11 13:21:19 +0700

    standard commit

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -2,13 +2,13 @@ export const prerender = false;
 import supabase from '$lib/supabaseClient.js';
 
 async function getBooks(q: string) {
-	const { data } = await supabase.from('books').select().textSearch('title', q);
+	const { data } = await supabase.from('books').select().textSearch('fts', q).limit(30);
 	// const { data } = await supabase.from('books').select('id, title').eq('id', 4);
 	return data;
 }
 
 async function getProducts(q: string) {
-	const { data } = await supabase.from('products').select().textSearch('name', q);
+	const { data } = await supabase.from('products').select().textSearch('fts', q).limit(30);
 	// const { data } = await supabase.from('books').select('id, title').eq('id', 4);
 	return data;
 }
