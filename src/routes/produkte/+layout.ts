c721921b5ce884cd7fcfb26338cf4f002b47f7e7
hanniboy <hanniboy@gmail.com>
2024-03-11 22:26:38 +0700

    standard commit

--- src/routes/produkte/+layout.ts
+++ src/routes/produkte/+layout.ts
@@ -11,12 +11,26 @@ const categories = [
 ];
 categories.sort((a, b) => a.name.localeCompare(b.name));
 */
+let cachedCategories = null;
+
+async function fetchCategories(){
+	if(!cachedCategories){
+		console.log('getting Categories from supabase');
+		const { data: categories } = await supabase.from('productcategories').select('*').order('name');
+		console.log('speichere Katgorien im Cache um zukünftige Anfragen zu beschleunigen');
+		cachedCategories = categories
+		return categories
+	}
+	console.log('Getting Categories from Cache');
+	return cachedCategories;
+}
 
 console.log('Produkt Layout wird initialisiert...');
 
 export async function load() {
 	const { count } = await supabase.from('products').select('*', { count: 'exact', head: true });
-	const { data: categories } = await supabase.from('productcategories').select('*').order('name');
+	const categories = await fetchCategories()
+
 	return {
 		count,
 		categories
