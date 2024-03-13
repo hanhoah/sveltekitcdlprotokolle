2d843c772795f0521184960254289515a0a5fc3d
hanniboy <hanniboy@gmail.com>
2024-03-13 23:55:23 +0700

    standard commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -9,6 +8,5 @@ const config = {
 		$img: 'src/lib/images'
 	},
 
-	preprocess: vitePreprocess()
 };
 export default config;
