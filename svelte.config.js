5e7504b6007e9068f90d710ee4dffac44142994a
hanniboy <hanniboy@gmail.com>
2024-03-13 23:56:57 +0700

    standard commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	alias: {
-		$img: 'src/lib/images'
-	},
-
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+    }),
+  },
 };
+ 
 export default config;
