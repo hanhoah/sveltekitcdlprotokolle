880ef0bd5c3ba63f512c617d8228e60e8560c84a
hanniboy <hanniboy@gmail.com>
2024-03-14 12:37:52 +0700

    standard commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,12 +3,14 @@ import { createClient } from '@supabase/supabase-js';
 let supabase;
 
 if (process.env.NODE_ENV === 'production') {
+	console.log('using production env');
 	// Konfiguration für die Produktionsumgebung
 	supabase = createClient(
 		'https://noejjknzqcfnwzgrpfdi.supabase.co',
 		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZWpqa256cWNmbnd6Z3JwZmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODMyNDQsImV4cCI6MjAyMzA1OTI0NH0.pEqKPgopedqGbawDnrNssT_RjZBhr-IJoUx_9uWWv1c'
 	);
 } else {
+	console.log('using dev env');
 	// Konfiguration für die lokale Entwicklungsumgebung
 	supabase = createClient(
 		'http://127.0.0.1:54321',
