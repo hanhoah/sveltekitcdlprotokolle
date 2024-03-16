5d32138347a92d4a0688dc71e56eed573a89164d
hanniboy <hanniboy@gmail.com>
2024-03-16 13:04:32 +0700

    standard commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,19 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-let supabase;
-
-if (process.env.NODE_ENV === 'production') {
-  // Konfiguration für die Produktionsumgebung
-  supabase = createClient(
+const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
   );
-} else {
-  // Konfiguration für die lokale Entwicklungsumgebung
-  supabase = createClient(
-    import.meta.env.VITE_SUPABASE_LOCAL_URL,
-    import.meta.env.VITE_SUPABASE_LOCAL_KEY
-  );
-}
 
+  
 export default supabase;
