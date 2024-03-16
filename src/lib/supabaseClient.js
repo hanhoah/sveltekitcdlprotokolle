3c47d1851e769e2d41b642907b1c952a1bce4cd8
hanniboy <hanniboy@gmail.com>
2024-03-16 12:06:46 +0700

    standard commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -5,9 +5,19 @@ import { createClient } from '@supabase/supabase-js';
 // 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZWpqa256cWNmbnd6Z3JwZmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODMyNDQsImV4cCI6MjAyMzA1OTI0NH0.pEqKPgopedqGbawDnrNssT_RjZBhr-IJoUx_9uWWv1c'
 // );
 
-const supabase = createClient(
-	process.env.SUPABASE_URL,
-	process.env.SUPABASE_KEY
-);
+
+if (process.env.NODE_ENV === 'production') {
+	// Konfiguration für die Produktionsumgebung
+	const supabase = createClient(
+		process.env.SUPABASE_URL,
+		process.env.SUPABASE_KEY
+	);
+	} else {
+	// Konfiguration für die lokale Entwicklungsumgebung
+	supabase = createClient(
+		process.env.DEV_SUPABASE_URL,
+		process.env.DEV_SUPABASE_KEY
+	);
+}
 
 export default supabase;
