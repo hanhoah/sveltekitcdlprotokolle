9220ae450ad990e21bf94a4ba692385603068b88
hanniboy <hanniboy@gmail.com>
2024-03-16 11:58:48 +0700

    standard commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
+// const supabase = createClient(
+// 	'https://noejjknzqcfnwzgrpfdi.supabase.co',
+// 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZWpqa256cWNmbnd6Z3JwZmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODMyNDQsImV4cCI6MjAyMzA1OTI0NH0.pEqKPgopedqGbawDnrNssT_RjZBhr-IJoUx_9uWWv1c'
+// );
+
 const supabase = createClient(
-	'https://noejjknzqcfnwzgrpfdi.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vZWpqa256cWNmbnd6Z3JwZmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODMyNDQsImV4cCI6MjAyMzA1OTI0NH0.pEqKPgopedqGbawDnrNssT_RjZBhr-IJoUx_9uWWv1c'
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_KEY
 );
 
 export default supabase;
