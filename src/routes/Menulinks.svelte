d2aa643da74b22b8758346f30e844d1e75224fab
hanniboy <hanniboy@gmail.com>
2024-11-16 12:18:09 +0700

    standard commit

--- src/routes/Menulinks.svelte
+++ src/routes/Menulinks.svelte
@@ -1,10 +1,22 @@
+<script>
+	let isOpen = false;
+
+	function closeMenu() {
+		isOpen = false;
+	}
+</script>
+
 <li><a href="/buecher">Bücher</a></li>
 <li>
-	<details>
+	<details bind:open={isOpen}>
 		<summary>CDL Protokolle</summary>
-		<ul class="bg-base-100 rounded-t-none p-2">
-			<li><a href="/cdl-protokolle">Basis Protokolle</a></li>
-			<li><a href="/cdl-protokolle/kombinierte-protokolle">Kombinierte Protokolle</a></li>
+		<ul class="bg-base-100 rounded-t-none p-2 dropdown-content">
+			<li><a href="/cdl-protokolle" on:click={closeMenu}>Basis Protokolle</a></li>
+			<li>
+				<a href="/cdl-protokolle/kombinierte-protokolle" on:click={closeMenu}
+					>Kombinierte Protokolle</a
+				>
+			</li>
 		</ul>
 	</details>
 </li>
@@ -13,3 +25,9 @@
 <li><a href="/leseproben">Leseproben</a></li>
 <li><a href="/krankheiten">Krankheiten</a></li>
 <li><a href="/artikel">Blog (Neu)</a></li>
+
+<style>
+	.dropdown-content {
+		z-index: 50; /* Setze einen hohen z-index-Wert */
+	}
+</style>
